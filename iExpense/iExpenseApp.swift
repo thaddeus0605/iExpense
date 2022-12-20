161b70f0153a04ea1faf69e8c061640be2a43427
thaddeus0605 <tdronski@gmail.com>
2022-12-20 12:00:10 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Thaddeus Dronski on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
